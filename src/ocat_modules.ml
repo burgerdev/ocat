fe8c27cb8599c07172d0339d04f9ed62c0dcc5d2
Markus Rudy <webmaster@burgerdev.de>
2018-01-30 21:22:37 +0100

    fix type of predefined monoid/monad instances

--- src/ocat_modules.ml
+++ src/ocat_modules.ml
@@ -3,46 +3,59 @@
 
 open Monoid
 
-module Int = Monoid(struct
-    type t = int
-    let zero = 0
-    let sum = (+)
-  end)
+module Int = struct
+  type t = int
+  include Monoid(struct
+      type t = int
+      let zero = 0
+      let sum = (+)
+    end)
+end
 
 module String = struct
   include (Monoid(struct
-    type t = string
-    let zero = ""
-    let sum = (^)
-  end): Monoid with type t := string)
+             type t = string
+             let zero = ""
+             let sum = (^)
+           end): Monoid with type t := string)
   include String
 end
 
-module Float = Monoid(struct
-    type t = float
-    let zero = 0.0
-    let sum = (+.)
-  end)
+module Float = struct
+  type t = float
+  include Monoid(struct
+      type t = float
+      let zero = 0.0
+      let sum = (+.)
+    end)
+end
 
 (** Monads *)
 
 open Monad
 
-module Id = Monad (
-  struct
-    type 'a t = 'a
-    let return x = x
-    let bind f x = f x
-  end)
+module Id = struct
+  type 'a t = 'a
+  include Monad (
+    struct
+      type 'a t = 'a
+      let return x = x
+      let bind f x = f x
+    end)
+end
 
-module Option = Monad (
-  struct
-    type 'a t = 'a option
-    let return x = Some x
-    let bind f x = match x with Some v -> f v | None -> None
-  end)
+module Option = struct
+  type 'a t = 'a option
+  include Monad (
+    struct
+      type 'a t = 'a option
+      let return x = Some x
+      let bind f x = match x with Some v -> f v | None -> None
+    end)
+end
 
 module List = struct
+  type 'a t = 'a list
   include Monad (
     struct
       type 'a t = 'a list
