dce2bbfff60cb8f38e3ec95d7482e193e491cf14
Markus Rudy <webmaster@burgerdev.de>
2018-02-03 14:36:53 +0100

    add paramorphism and histomorphism

--- src/fix.ml
+++ src/fix.ml
@@ -1,24 +1,41 @@
 open Functor
+open Cofree
+
 
 module type Fix = sig
   type 'a t
 
+  type 'a algebra = 'a t -> 'a
+  type 'a coalgebra = 'a -> 'a t
+
   type 'a fix = Fix of ('a fix) t
+
+  module C: Cofree with type 'a f := 'a t
 
   val fix: 'a fix t -> 'a fix
   val unfix: 'a fix -> 'a fix t
 
   val fixed_map: ('a -> 'b) -> 'a fix -> 'b fix
 
-  val cata: ('a t -> 'a) -> 'a fix -> 'a
+  val cata: 'a algebra -> 'a fix -> 'a
 
-  val ana: ('a -> 'a t) -> 'a -> 'a fix
+  val ana: 'a coalgebra -> 'a -> 'a fix
 
-  val hylo: ('a -> 'a t) -> ('b t -> 'b) -> 'a -> 'b
+  val para: (('a fix * 'a) t -> 'a) -> 'a fix -> 'a
+
+  val hylo: 'a coalgebra -> 'b algebra -> 'a -> 'b
+
+  val histo: ('a C.t t -> 'a) -> 'a fix -> 'a
+
 end
 
 module Fix (F: Functor_base): Fix with type 'a t := 'a F.t = struct
   open F
+
+  module C = Cofree(F)
+
+  type 'a algebra = 'a t -> 'a
+  type 'a coalgebra = 'a -> 'a t
 
   type 'a fix = Fix of ('a fix) t
 
@@ -28,14 +45,28 @@ module Fix (F: Functor_base): Fix with type 'a t := 'a F.t = struct
   let rec fixed_map f a_fix =
     unfix a_fix |> map @@ fixed_map f |> fix
 
-  let rec cata: ('a t -> 'a) -> 'a fix -> 'a = fun alg -> fun fix_x ->
-    unfix fix_x
+  let rec cata alg a_fix =
+    unfix a_fix
     |> map @@ cata alg
     |> alg
 
-  let rec ana: ('a -> 'a t) -> 'a -> 'a fix = fun coalg -> fun x ->
-    coalg x |> map (ana coalg) |> fix
+  let rec ana coalg a =
+    coalg a |> map (ana coalg) |> fix
 
-  let rec hylo: ('a -> 'a t) -> ('b t -> 'b) -> 'a -> 'b = fun coalg alg -> fun x ->
-    coalg x |> map (hylo coalg alg) |> alg
+  let rec hylo coalg alg a =
+    coalg a |> map (hylo coalg alg) |> alg
+
+  let rec para p_alg a_fix =
+    unfix a_fix
+    |> map (para p_alg)
+    |> map (fun a -> (a_fix, a))
+    |> p_alg
+
+  let rec histo: ('a C.t F.t -> 'a)  -> 'a fix -> 'a = fun c_alg a_fix ->
+    unfix a_fix
+    |> map (delegate c_alg)
+    |> c_alg
+  and delegate = fun c_alg a_fix ->
+    let tail = unfix a_fix |> map (delegate c_alg) in
+    Cofree (histo c_alg a_fix, tail)
 end
