ffbad375936a7c74c4177660ecd52a2184468e4e
Markus Rudy <webmaster@burgerdev.de>
2018-02-04 21:16:47 +0100

    make cofree lazy in its tail

--- src/cofree.ml
+++ src/cofree.ml
@@ -1,10 +1,11 @@
 
 open Comonad
 open Functor
+open Ocat_modules
 
 module type Cofree = sig
   type 'a f
-  type 'a t = Cofree of 'a * 'a t f
+  type 'a t = Cofree of 'a * 'a t f Lazy.t
 
   include Comonad with type 'a t := 'a t
 end
@@ -12,15 +13,17 @@ end
 module Cofree (F: Functor_base): Cofree with type 'a f := 'a F.t = struct
 
   module M = struct
-    type 'a t = Cofree of 'a * 'a t F.t
+    type 'a t = Cofree of 'a * 'a t F.t Lazy.t
 
-    let extract = function Cofree (a, _) -> a
+    let extract = function
+      | Cofree (a, _) -> a
+
     let rec extend f = function
-      | Cofree (a, tail) as c -> Cofree (f c, F.map (extend f) tail)
+      | Cofree (a, tail) as c ->
+        Cofree (f c, Lazy.(map F.(map (extend f)) tail))
   end
 
   include Comonad(M)
-
   include M
 
 end
