7b28499932ececc18e36dc3e16d07ff08738ebfa
Markus Rudy <webmaster@burgerdev.de>
2018-02-05 14:34:22 +0100

    add functor map infix notation

--- src/functor.ml
+++ src/functor.ml
@@ -9,10 +9,13 @@ module type Functor = sig
   include Functor_base
 
   val foreach: ('a -> unit) -> 'a t -> unit
+  val (>|=): 'a t -> ('a -> 'b) -> 'b t
 end
 
 module Functor (F: Functor_base): Functor with type 'a t := 'a F.t = struct
   include F
 
   let foreach f a = map f a |> ignore
+
+  let (>|=) a f = map f a
 end
