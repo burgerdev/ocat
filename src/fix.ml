65eab659addc08883d556685571cef1cd0306000
Markus Rudy <webmaster@burgerdev.de>
2018-02-07 22:43:27 +0100

    add zygo to Fix

--- src/fix.ml
+++ src/fix.ml
@@ -14,6 +14,7 @@ module type Fix = sig
   val cata: ('a t -> 'a) -> 'b fix -> 'a
   val para: (('b fix * 'a) t -> 'a) -> 'b fix -> 'a
   val histo: ('a C.t t -> 'a) -> 'b fix -> 'a
+  val zygo: ('b t -> 'b) -> (('b * 'a) t -> 'a) -> 'c fix -> 'a
 
   val ana: ('a -> 'a t) -> 'a -> 'b fix
   (* TODO apo *)
@@ -56,5 +57,16 @@ module Fix (F: Functor_base): Fix with type 'a t := 'a F.t = struct
       |> fun x -> C.Cofree (c_alg x, lazy x)
     in
     aux a_fix |> C.extract
+
+  let zygo alg pair_alg a_fix =
+    let rec aux a_fix =
+      let f_a = unfix a_fix in
+      let mapped = map aux f_a in
+      let b = mapped |> map fst |> alg in
+      let a = pair_alg mapped in
+      (b, a)
+    in
+    aux a_fix
+    |> snd
 
 end
